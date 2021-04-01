1f58d0d553ef857db2cc4c05240ecb45f18c91cb
icecold120000 <61144437+icecold120000@users.noreply.github.com>
2021-04-01 17:12:14 +0200

    Mise à jour du BLL

---
+++ BLL/BLL/GestionFlux.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BO;
+using DAL;
+
+namespace BLL
+{
+    public class GestionFlux
+    {
+        private static GestionFlux uneGestionFlux; // objet BLL
+
+        
+        public static GestionFlux GetGestionFlux()
+        {
+            if (uneGestionFlux == null)
+            {
+                uneGestionFlux = new GestionFlux();
+            }
+            return uneGestionFlux;
+        }
+
+         public static void SetchaineConnexion(ConnectionStringSettings chset)
+         {
+            string chaine = chset.ConnectionString;
+            ConnexionBD.GetConnexionBD().SetchaineConnexion(chaine);
+         }
+
+        public static List<Flux> GetFlux(int idType)
+        {
+            return FluxDAO.GetFlux(idType);
+        }
+
+        public static void AjoutFlux(Flux unFlux, SqlConnection myConnection)
+        {
+            FluxDAO.AjouterFlux(unFlux, myConnection);
+        }
+
+        public static void DeleteFlux(int idFlux)
+        {
+            FluxDAO.DeleteFlux(idFlux);
+        }
+
+
+        public static void UpdateFlux(Flux unFlux, SqlConnection myConnection)
+        {
+            FluxDAO.UpdateFlux(unFlux, myConnection);
+        }
+    }
+}
---
+++ BLL/BLL/GestionUtilisateur.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BO;
+using DAL;
+
+namespace BLL
+{
+    public class GestionUtilisateurs
+    {
+        private static GestionUtilisateurs uneGestionUtilisateurs; // objet BLL
+
+        // Accesseur en lecture
+        public static GestionUtilisateurs GetGestionUtilisateurs()
+        {
+            if (uneGestionUtilisateurs == null)
+            {
+                uneGestionUtilisateurs = new GestionUtilisateurs();
+            }
+            return uneGestionUtilisateurs;
+        }
+
+        // Définit la chaîne de connexion grâce à la méthode SetchaineConnexion
+        //de la DAL
+        public static void SetchaineConnexion(ConnectionStringSettings chset)
+        {
+            string chaine = chset.ConnectionString;
+            ConnexionBD.GetConnexionBD().SetchaineConnexion(chaine);
+        }
+        // Méthode qui renvoit une List d'objets Utilisateur en faisant appel à
+        //la méthode GetUtilisateurs() de la DAL
+        public List<Utilisateur> GetUtilisateurs()
+        {
+            return UtilisateurDAO.GetUtilisateurs();
+        }
+
+        public static void Connexion(string utiLogin, string utiMpd,
+            string utiDroit, SqlConnection maConnexion)
+        {
+             UtilisateurDAO.Requete(utiLogin, utiMpd,
+             utiDroit, maConnexion);
+        }
+
+    }
+}
